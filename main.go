5811e7a70f5a2bc35e6088e780eed18de6d28081
ed <ab8bas@gmail.com>
2023-06-05 18:11:27 +1000

    added main file

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+)
+
+//go:embed ui/dist/pwa/*
+var embedFS embed.FS
+
+func main() {
+	app := fiber.New()
+
+	// embed ui into program binary
+	app.Use("/app", filesystem.New(filesystem.Config{
+		Root:       http.FS(embedFS),
+		PathPrefix: "app",
+		Browse:     true,
+	}))
+
+	port := flag.Int("port", 3000, "")
+	log.Fatal(app.Listen(":" + fmt.Sprint(port)))
+}
