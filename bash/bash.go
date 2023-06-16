c84e5996230d1b6392c6a274e8294990111b4afd
ed <ab8bas@gmail.com>
2023-06-16 23:25:11 +1000

    renamed function

--- bash/bash.go
+++ bash/bash.go
@@ -7,7 +7,7 @@ import (
 	"github.com/evdnx/linux-control/auth"
 )
 
-func Exec(command string) *exec.Cmd {
+func Cmd(command string) *exec.Cmd {
 	c := fmt.Sprint("echo ", auth.Password, " | su - ", auth.Username, " -c ", command)
 	return exec.Command("bash", "-c", c)
 }
