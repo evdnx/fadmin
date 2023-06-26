3f2531d3b8a0de2532ad326bf3fb88059c5fd18e
ed <ab8bas@gmail.com>
2023-06-26 17:37:58 +1000

    refactor

--- pkg/crypt/crypt.go
+++ pkg/crypt/crypt.go
@@ -3,53 +3,43 @@ package crypt
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
 	"encoding/base64"
-	"fmt"
-	"io"
 )
 
 // Encrypt encrypts a message
 func Encrypt(key []byte, message string) (string, error) {
-	byteMsg := []byte(message)
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", fmt.Errorf("could not create new cipher: %v", err)
+		return "", err
 	}
 
-	cipherText := make([]byte, aes.BlockSize+len(byteMsg))
+	plainText := []byte(message)
+	cipherText := make([]byte, len(plainText))
+
 	iv := cipherText[:aes.BlockSize]
-	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-		return "", fmt.Errorf("could not encrypt: %v", err)
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(cipherText[aes.BlockSize:], byteMsg)
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(cipherText, plainText)
 
 	return base64.StdEncoding.EncodeToString(cipherText), nil
 }
 
 // Decrypt decrypts a message
 func Decrypt(key []byte, message string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
 	cipherText, err := base64.StdEncoding.DecodeString(message)
 	if err != nil {
-		return "", fmt.Errorf("could not base64 decode: %v", err)
-	}
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", fmt.Errorf("could not create new cipher: %v", err)
-	}
-
-	if len(cipherText) < aes.BlockSize {
-		return "", fmt.Errorf("invalid ciphertext block size")
+		return "", err
 	}
 
 	iv := cipherText[:aes.BlockSize]
-	cipherText = cipherText[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
 
-	stream := cipher.NewCFBDecrypter(block, iv)
-	stream.XORKeyStream(cipherText, cipherText)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
 
-	return string(cipherText), nil
+	return string(plainText), nil
 }
