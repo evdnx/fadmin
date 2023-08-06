621b1a8aa4f0e30ef18760c0851a4105dd66ef12
ed <ab8bas@gmail.com>
2023-08-06 21:57:23 +1000

    added read and update methods

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/evdnx/unixmint/constants"
@@ -31,6 +32,43 @@ func Init() error {
 
 func DB() *bbolt.DB {
 	return db
+}
+
+func getBucket(tx *bbolt.Tx, bucket string) (*bbolt.Bucket, error) {
+	b := tx.Bucket([]byte(bucket))
+	if b == nil {
+		return nil, errors.New("bucket " + bucket + " doesn't exist")
+	}
+
+	return b, nil
+}
+
+func Read(bucket, key string) (string, error) {
+	value := ""
+	err := DB().View(func(tx *bbolt.Tx) error {
+		b, err := getBucket(tx, bucket)
+		if err != nil {
+			return err
+		}
+
+		v := b.Get([]byte(key))
+		value = string(v)
+		return nil
+	})
+
+	return value, err
+}
+
+func Update(bucket, key, value string) error {
+	return DB().Update(func(tx *bbolt.Tx) error {
+		b, err := getBucket(tx, bucket)
+		if err != nil {
+			return err
+		}
+
+		err = b.Put([]byte(key), []byte(value))
+		return err
+	})
 }
 
 func AuthBucket() {
