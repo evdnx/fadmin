7c006ea89146bf85aaf5f50df313f7f07b6752e5
ed <ab8bas@gmail.com>
2023-11-16 22:40:20 +1100

    switched auth middleware to echo

--- middleware/auth.middleware.go
+++ middleware/auth.middleware.go
@@ -1,25 +1,29 @@
 package middleware
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 
 	"github.com/evdnx/unixmint/auth"
 )
 
-func AuthMiddleware() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		// check if Authorization header exists in the request
-		authHeader := c.Get("Authorization", "")
-		if authHeader == "" {
-			return c.SendStatus(fiber.StatusUnauthorized)
-		}
+func AuthMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// check if Authorization header exists in the request
+			authHeader := c.Request().Header.Get("Authorization")
+			if authHeader == "" {
+				return c.NoContent(http.StatusUnauthorized)
+			}
 
-		// decode token brancaToken
-		_, err := auth.DecodeToken(authHeader, 24, nil)
-		if err != nil {
-			return c.SendStatus(fiber.StatusUnauthorized)
-		}
+			// decode token brancaToken
+			_, err := auth.DecodeToken(authHeader, 24, nil)
+			if err != nil {
+				return c.NoContent(http.StatusUnauthorized)
+			}
 
-		return c.Next()
+			return next(c)
+		}
 	}
 }
